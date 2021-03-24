778edb6528eb6f1bb8ba5c21fab9100c63cff78b
franzudev <fede.franzu@gmail.com>
2021-03-24 15:30:12 +0100

    update sqlfiles

--- srcs/mysql/management.sql
+++ srcs/mysql/management.sql
@@ -1,5 +1,8 @@
 GRANT ALL PRIVILEGES ON *.* TO 'mysql'@'%' IDENTIFIED BY 'mysql' WITH GRANT OPTION;
 
+CREATE DATABASE wordpress;
+CREATE USER 'wordpress'@'%' IDENTIFIED BY 'wordpress';
+GRANT ALL PRIVILEGES ON wordpress.* TO "wordpress"@"%" with GRANT OPTION;
 CREATE USER 'phpadmin'@'%' IDENTIFIED BY 'superCaliFragiliSticoESpiraLidoso';
 GRANT ALL PRIVILEGES ON `phpmyadmin`.* TO 'phpadmin'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
