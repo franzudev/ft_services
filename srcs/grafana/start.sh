00278bd5ece967da94843d1dad925ac93f5feb2c
franzudev <fede.franzu@gmail.com>
2021-03-29 20:23:52 +0200

    add grafana and influx, implement telegraf in other containers

--- srcs/grafana/start.sh
+++ srcs/grafana/start.sh
@@ -1,4 +1,13 @@
 # openrc default
 # openrc boot
 
+curl "http://influxdb:8086/query" --data-urlencode "q=CREATE USER telegraf
+ WITH PASSWORD 'telegraf' WITH ALL PRIVILEGES"
+
+curl "http://influxdb:8086/query" --data-urlencode "q=CREATE DATABASE telegraf"
+
+telegraf_init
+
+./telegraf/usr/bin/telegraf --config /telegraf/etc/telegraf/telegraf.conf &
+
 cd grafana && ./bin/grafana-server
--- srcs/influxdb/start.sh
+++ srcs/influxdb/start.sh
@@ -1,3 +1,5 @@
+telegraf_init
 
+./telegraf/usr/bin/telegraf --config /telegraf/etc/telegraf/telegraf.conf &
 
 influxd
